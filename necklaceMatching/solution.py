ac5ad318abed39766fca999233f95f900efdcd46
Andrew Lehman <aplehm@gmail.com>
2022-08-02 19:43:40 -0400

    Add solution

---
+++ necklaceMatching/solution.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+def same_necklace(necklace1, necklace2):
+    if len(necklace1) != len(necklace2):
+        return False
+
+    if necklace1 == necklace2:
+        return True
+
+    # If we can move one string around to be the other then we have a match!
+    # We need to keep track of the initial state so we know when we've repeated
+    tempNecklace = necklace1
+    i = 1
+
+    while i <= len(necklace1):
+        # move a letter to end and check against necklace2
+        slicedBeads = tempNecklace[i:] + tempNecklace[:i]
+
+        if slicedBeads == necklace2:
+            return True
+
+        i += 1
+
+    return False
+
+print(same_necklace("nicole", "icolen"))
+print(same_necklace("nicole", "lenico"))
+print(same_necklace("nicole", "coneli"))
+print(same_necklace("aabaaaaabaab", "aabaabaabaaa"))
+print(same_necklace("abc", "cba"))
+print(same_necklace("xxyyy", "xxxyy"))
+print(same_necklace("xyxxz", "xxyxz"))
+print(same_necklace("x", "x"))
+print(same_necklace("x", "xx"))
+print(same_necklace("x", ""))
+print(same_necklace("", ""))
