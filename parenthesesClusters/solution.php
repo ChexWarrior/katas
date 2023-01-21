0d8a7279ce8aea6c5c7e520ef7c246946801bfef
Andrew Lehman <aplehm@gmail.com>
2023-01-21 18:49:30 -0500

    Add solution for last challenge

--- parenthesesClusters/solution.php
+++ parenthesesClusters/solution.php
@@ -1,3 +1,29 @@
 <?php
 
+function split($initial) {
+    $target = str_split($initial);
+    $result = [];
 
+    if (empty($initial)) return [];
+
+    $tally = 0;
+    $temp = '';
+    foreach ($target as $char) {
+        if ($char === '(') $tally += 1;
+        if ($char === ')') $tally -= 1;
+
+        $temp .= $char;
+
+        if ($tally === 0) {
+            $result[] = $temp;
+            $temp = '';
+        }
+    }
+
+    return implode(',', $result);
+}
+
+echo split("()()()"), PHP_EOL;
+echo split("((()))"), PHP_EOL;
+echo split("((()))(())()()(()())"), PHP_EOL;
+echo split("((())())(()(()()))"), PHP_EOL;
