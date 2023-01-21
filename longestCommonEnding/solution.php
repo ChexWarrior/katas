8831f7caac15677be8ee1753bf538985ec3ef618
Andrew Lehman <aplehm@gmail.com>
2023-01-21 09:48:59 -0500

    Add solution

--- longestCommonEnding/solution.php
+++ longestCommonEnding/solution.php
@@ -1,3 +1,24 @@
 <?php
 
+function longestCommonEnding(string $a, string $b): string {
+    // Go through strings in reverse
 
+    // Compare each character, continue until difference, return like string
+
+    $commonEnding = '';
+    $a_array = array_reverse(str_split($a));
+    $b_array = array_reverse(str_split($b));
+
+    for ($i = 0; $i < count($a_array) && $i < count($b_array); $i += 1) {
+        if ($a_array[$i] === $b_array[$i]) {
+            $commonEnding = $a_array[$i] . $commonEnding;
+            continue;
+        }
+
+        break;
+    }
+
+    return $commonEnding;
+}
+
+echo longestCommonEnding("trick", "truck"), PHP_EOL;
