1e2d97c597326ffedc2fc8a9204e160bf60fe091
chexwarrior <aplehm@gmail.com>
2023-01-22 15:14:28 -0500

    Maybe the start of a recursive solution

--- frequencyByLevelOfNesting/solution.php
+++ frequencyByLevelOfNesting/solution.php
@@ -1,3 +1,32 @@
 <?php
 
+function freqCount($nested, $element) {
+    return unwind($nested, $element, 0, 0, [[0,0]]);
+}
+
+function unwind($group, $element, $level, $index, $occurrences) {
+    $item = $group[$index];
+
+    if (!isset($item)) {
+        return $occurrences;
+    }
+
+    if (is_array($item)) {
+        $occurrences[] = [$level + 1, 0];
+        return unwind($item, $element, $level + 1, 0, $occurrences);
+    } else {
+        if ($item === $element) {
+            $count = $occurrences[$level][1] + 1;
+            $occurrences[$level] = [$level, $count];
+        }
+
+        return unwind(
+            $item,
+            $element,
+            $level,
+            $index + 1,
+            $occurrences,
+        );
+    }
+}
 
